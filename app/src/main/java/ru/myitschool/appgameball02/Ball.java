1ab3370c021f5c06741a41aec854b1ed9811dfc9
Roman Malyshkov <malyshkov.roman@gmail.com>
2016-11-08 01:54:31 +0700

    Working on particles. Timer and points are now correctly working. Changed random from Math.random to java.util.Random

--- app/src/main/java/ru/myitschool/appgameball02/Ball.java
+++ app/src/main/java/ru/myitschool/appgameball02/Ball.java
@@ -1,20 +1,35 @@
 package ru.myitschool.appgameball02;
 
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
+import com.plattysoft.leonids.ParticleSystem;
+
+import java.util.Random;
+
 public class Ball {
 
-    private int x, y,speedX, speedY;
-    private int screenH, screenW;
+    private int x;
+    private int y;
+    private int speedX;
+    private int speedY;
+    private int screenH;
+    private int screenW;
     private ImageView image;
 
+    private MainActivity activity;
+
+    public static int points = 0;
+
     private static int size;
-    public static int countTouch;
 
-    public Ball(MainActivity activity, int screenH, int screenW,
-                int beginSpeed, int endSpeed, int size){
 
+    Ball(MainActivity activity, int screenH, int screenW,
+                int beginSpeed, int endSpeed, int size) {
+
+        this.activity = activity;
         this.screenH = screenH;
         this.screenW = screenW;
         this.image = new ImageView(activity);
@@ -23,15 +38,18 @@ public class Ball {
         this.x = (int) (Math.random() * screenH / 2);
         this.y = (int) (Math.random() * screenW / 2);
 
+        Random random = new Random();
+
         activity.addContentView(image,
                 new RelativeLayout.LayoutParams(size, size));
 
         image.setX(x);
         image.setY(y);
         image.setImageResource(R.drawable.ball);
+        image.setOnTouchListener(imageTouch);
 
-        this.speedX = (int) (beginSpeed + (Math.random() * (endSpeed - beginSpeed) + 1));
-        this.speedY = (int) (beginSpeed + (Math.random() * (endSpeed - beginSpeed) + 1));
+        this.speedX = random.nextInt(endSpeed - beginSpeed) + beginSpeed + 1;
+        this.speedY = random.nextInt(endSpeed - beginSpeed) + beginSpeed + 1;
     }
 
     public void move(){
@@ -45,4 +63,22 @@ public class Ball {
         if (y < 0 || y > screenH - size)
             speedY = - speedY;
     }
+
+    private View.OnTouchListener imageTouch = new View.OnTouchListener() {
+
+        @Override
+        public synchronized boolean onTouch(View view, MotionEvent motionEvent) {
+            if (MainActivity.balls.contains(Ball.this)) {
+                int numParticles = 10;
+                MainActivity.textScore.setText(view.getResources().getString(R.string.points, ++Ball.points));
+                /* new ParticleSystem(activity, numParticles, R.drawable.particle1, 300L)
+                        .setSpeedRange(0.2f, 0.5f)
+                        .oneShot(view, numParticles); */
+                MainActivity.balls.remove(Ball.this);
+                image.setVisibility(View.GONE);
+                return true;
+            }
+            return false;
+        }
+    };
 }
--- app/src/main/java/ru/myitschool/appgameball02/MainActivity.java
+++ app/src/main/java/ru/myitschool/appgameball02/MainActivity.java
@@ -4,6 +4,9 @@ import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -11,12 +14,18 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private List<Ball> balls = null;
-    private int countBalls = 5;
+    protected static List<Ball> balls = null;
+    protected static int countBalls = 5;
 
-    private TextView textTimer, textScore, textTouch;
+    private TextView textTimer;
+    protected static TextView textScore;
+    private TextView textTouch;
     private int screenH, screenW;
     private MyTimer timer = null;
+
+    private int countTouch = 0;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +35,10 @@ public class MainActivity extends AppCompatActivity {
         textTimer = (TextView) findViewById(R.id.text_timer);
         textScore = (TextView) findViewById(R.id.text_score);
         textTouch = (TextView) findViewById(R.id.text_touch);
+
+        textScore.setText(getResources().getString(R.string.points, 0));
+        textTimer.setText(getResources().getString(R.string.timer, 10));
+        textTouch.setText(getResources().getString(R.string.touches, 0));
 
         DisplayMetrics displayMetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
@@ -54,10 +67,12 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onTick(long l) {
             moveBalls();
+            textTimer.setText(getResources().getString(R.string.timer, (int)l/1000));
         }
 
         @Override
         public void onFinish() {
+            Ball.points = 0;
             finish();
         }
     }
@@ -66,5 +81,11 @@ public class MainActivity extends AppCompatActivity {
         for (Ball ball : balls){
             ball.move();
         }
+    }
+
+    @Override
+    protected void onRestart() {
+        Ball.points = 0;
+        super.onRestart();
     }
 }
